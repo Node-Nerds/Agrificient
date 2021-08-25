46e416e0e3a7c04799a16407231bda9ccfa6d18c
Rutvij Vamja <rutvij.vamja@gmail.com>
2021-08-25 16:26:51 +0530

    fix

--- routes/protected/warehouse/index.js
+++ routes/protected/warehouse/index.js
@@ -4,7 +4,6 @@ const warehouseHome = require("./home");
 const addWarehouse = require("./addWarehouse");
 const searchWarehouse = require("./searchWarehouse");
 
-
 router.use("/home", warehouseHome);
 router.use("./addWarehouse", addWarehouse);
 router.use("./searchWareHouse", searchWarehouse);
