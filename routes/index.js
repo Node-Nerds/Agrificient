da92a0507c1344b46346885b43874df5fe0b78a4
Rutvij Vamja <rutvij.vamja@gmail.com>
2021-08-25 15:26:47 +0530

    warehouse

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ const router = require("express").Router();
 
 const addwarehouse = require("./protected/addWarehouse");
 const searchwarehouse = require("./protected/searchWarehouse");
+const warehouse = require("./protected/warehouse");
+const proposal = require("./protected/proposal");
 
 router.use("/", addwarehouse);
 router.use("/", searchwarehouse);
+router.use("/", warehouse);
+router.use("/", proposal);
 
 module.exports = router;
