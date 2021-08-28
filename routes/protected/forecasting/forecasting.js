a79814181355328edf7a7604cea2b777b5c248a3
Rutvij Vamja <rutvij.vamja@gmail.com>
2021-08-28 21:23:37 +0530

    Fix

---
+++ routes/protected/forecasting/forecasting.js
@@ -0,0 +1,30 @@
+let { PythonShell } = require("python-shell");
+const router = require("express").Router();
+
+router.get("/", (req, res) => {
+  res.render("forecasting/forecasting.ejs");
+});
+
+router.post("/", (req, res) => {
+  var options = {
+    mode: "text",
+    pythonPath: "python",
+    pythonOptions: ["-u"],
+    scriptPath: "D:/Web/Agrificient/PyScript",
+    args: [
+      "--year=" + req.body.year,
+      "--season=" + req.body.season,
+      "--district=" + req.body.district,
+      "--crop=" + req.body.crop,
+    ],
+  };
+
+  PythonShell.run("predict.py", options, function (err, results) {
+    if (err) throw err;
+    // results is an array consisting of messages collected during execution
+
+    res.send(results);
+  });
+});
+
+module.exports = router;
